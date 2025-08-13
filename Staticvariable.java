7883f769a72076df197e8163733ed84675fe1d46
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-08-13 23:06:49 +0530

    Initial Commit

---
+++ Staticvariable.java
@@ -0,0 +1,35 @@
+ class Que_12 {
+    static int count=0;
+
+    void setcount(int val){
+        count=val;
+    }
+    
+    void displayCount(){
+        System.out.println("Count => "+count);
+    }
+
+}
+
+public class Staticvariable {
+    public static void main(String[] args) {
+        Que_12 obj1=new Que_12();
+        Que_12 obj2=new Que_12();
+
+        obj1.setcount(20);
+        System.out.println("from obj1:");
+        obj1.displayCount();
+
+        System.out.println("from obj2:");
+        obj2.displayCount();
+
+        obj2.setcount(40);
+        System.out.println("from the obj1 after change in obj2:");
+        obj1.displayCount();
+
+        System.out.println("from obj2 after the change");
+        obj2.displayCount();
+    }
+
+    
+}
