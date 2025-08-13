46155bee1b9e9f1d8ec37209a7a2e4fcd87461cc
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-08-13 10:52:57 +0530

    Initial Commit

--- Que_8.java
+++ Que_8.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Que_8 {
-    
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the base");
+        int base=sc.nextInt();
+        System.out.println("Enter the exponent");
+        int exponent=sc.nextInt();
+        
+        int power=(int)Math.pow(base, exponent);
+
+        System.out.println("power of given base and exponent => " +power);
+    }
 }
