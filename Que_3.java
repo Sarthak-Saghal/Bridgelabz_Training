927ad56f3c23a16c6831b0a55a1c4c07054b3706
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-08-13 10:11:58 +0530

    Initial Commit

---
+++ Que_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Que_3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the temperature in celsius");
+        float temp_Celsius= sc.nextInt();
+
+        float temp_fahrenheit=(temp_Celsius*9/5)+32;
+
+        System.out.println("Temperature in fahrenheit => " +temp_fahrenheit);
+    }
+}
