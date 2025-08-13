0e0fc16667adc2deb1d0fcaab5cf137b53f98416
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-08-13 11:57:44 +0530

    intial commit

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        QueCar_11 car1= new QueCar_11("Toyota", 180);
-        
-        
-        System.out.println(car1);
-    }
-}
