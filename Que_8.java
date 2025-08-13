d5e1c43d39bd8261da104e63c0d91e5372ea368a
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-08-13 10:47:24 +0530

    Initial Commit

--- Que_8.java
+++ Que_8.java
@@ -1,14 +1,3 @@
-import java.util.Scanner;
-
 public class Que_8 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the length");
-        float length=sc.nextFloat();
-        System.out.println("Enter the width");
-        float width=sc.nextFloat();
-        
-        float perimeter=2*(length+width);
-        System.out.println("perimeter of rectangle =>" + perimeter);
-    }
+    
 }
