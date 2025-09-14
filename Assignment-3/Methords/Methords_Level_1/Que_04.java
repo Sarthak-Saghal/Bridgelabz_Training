f5d768014dba895d8afca308cb0cb38ead3ac65a
Sarthak-Saghal <sarthaksahgal2005@gmail.com>
2025-09-15 02:36:25 +0530

    Methords

---
+++ Assignment-3/Methords/Methords_Level_1/Que_04.java
@@ -0,0 +1,22 @@
+﻿import java.util.Scanner;
+
+public class Que_04 {
+
+    public static int calculateHandshakes(int nStudents) {
+        return (nStudents * (nStudents - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int nStudents = sc.nextInt();
+
+        int handshakes = calculateHandshakes(nStudents);
+
+        System.out.println("The maximum number of handshakes among " + nStudents +
+                " students is: " + handshakes);
+
+        sc.close();
+    }
+}
---
+++ Assignment-3/Methords/Methords_Level_2/Que_01.java
@@ -0,0 +1,76 @@
+
+
+import java.util.Scanner;
+
+public class Que_01 {
+
+    public static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        return factors;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int factor : factors) {
+            sum += factor;
+        }
+        return sum;
+    }
+
+    public static long productOfFactors(int[] factors) {
+        long product = 1;
+        for (int factor : factors) {
+            product *= factor;
+        }
+        return product;
+    }
+
+    public static int sumOfSquares(int[] factors) {
+        int sumSquares = 0;
+        for (int factor : factors) {
+            sumSquares += factor * factor; // changed from Math.pow to avoid casting issues
+        }
+        return sumSquares;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] factors = findFactors(number);
+
+        System.out.print("Factors of " + number + " are: ");
+        for (int factor : factors) {
+            System.out.print(factor + " ");
+        }
+        System.out.println();
+
+        int sum = sumOfFactors(factors);
+        System.out.println("Sum of factors: " + sum);
+
+        long product = productOfFactors(factors);
+        System.out.println("Product of factors: " + product);
+
+        int sumSquares = sumOfSquares(factors);
+        System.out.println("Sum of squares of factors: " + sumSquares);
+
+        sc.close();
+    }
+}
